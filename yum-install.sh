47e588217bec8f363561bb333078165c7b122946
Petr Demin <petr_demin@epam.com>
2015-06-05 15:22:45 +0300

    install python-devel for coverage

--- yum-install.sh
+++ yum-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 yum install -y epel-release
-yum install -y gcc python-pip
+yum install -y gcc python-pip python-devel
 yum install -y git colordiff screen vim
 yum install -y python-ordereddict yum-utils python-six python-backports python-backports-ssl_match_hostname python-urllib3 python-babel python-jinja2 python-chardet python-requests sshpass python-crypto m2crypto libyaml PyYAML python-msgpack openpgm zeromq3 python-zmq htop iotop python-m2ext python-dns python-unittest2 python-paramiko
 yum install -y /vagrant/salt*
