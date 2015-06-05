91ba786300d01fa2f44251bd4a52a14799f42302
Petr Demin <petr_demin@epam.com>
2015-06-05 13:48:49 +0300

    initial commit

---
+++ pip-install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -ev
+
+pip install argparse
+pip install astroid
+pip install coverage
+pip install kazoo
+pip install linecache2
+pip install logilab-common
+pip install mock
+pip install pbr
+pip install py
+pip install pylint
+pip install pytest
+pip install pyudev
+pip install texttable
+pip install traceback2
+pip install ecdsa
+pip install paramiko
